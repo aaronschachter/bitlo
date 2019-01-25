a248959d9d8a786c0c46db3d45dc9cfe3fa6d17e
Aaron Schachter <aaronschachter@gmail.com>
2019-01-25 11:03:12 -0800

    Displays list of short links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
 
+const BITLY_BASE_URI = 'https://api-ssl.bitly.com/v4';
+
+// Insert token here.
+const token = 'fake';
+
+const headers = {
+  Authorization: `Bearer ${token}`,
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+      shortenedLinks: [],
+    };
+    this.shortenLink = this.shortenLink.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentWillMount() {
+    // Fetch user.
+    fetch(`${BITLY_BASE_URI}/user`, { method: 'GET', headers })
+      .then(res => res.json())
+      .then((user) => {
+        this.setState({ user });
+      });
+  }
+  handleChange({ target }) {
+    this.setState({ longUrl: target.value });
+  }
+  shortenLink() {
+    const body = JSON.stringify({ long_url: this.state.longUrl });
+    console.log(body);
+    return fetch(`${BITLY_BASE_URI}/shorten`, { method: 'POST', headers, body })
+      .then(res => res.json())
+      .then((json) => {
+        const temp = this.state.shortenedLinks;
+        temp.push(json.link);
+        this.setState({ shortenedLinks: temp });
+      });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Login: {this.state.user.login}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>
+            <input 
+              type="text" 
+              name="longUrl" 
+              placeholder="Enter URL here..." 
+              value={ this.state.longUrl }
+              onChange={ this.handleChange } 
+            />
+            <button onClick={ this.shortenLink }>Shorten</button>
+          </p>
+          <ul>
+            {this.state.shortenedLinks.map(item => <li>{item}</li>)}
+          </ul>
         </header>
       </div>
     );
